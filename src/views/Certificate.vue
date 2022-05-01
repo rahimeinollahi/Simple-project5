315daa68310959c96e0375bd17c26b03d56a6b8d
rahim einollahi <r.einollahi@gamil.com>
2022-05-01 20:49:53 +0430

    Certificate Route too added to project

--- src/views/Certificate.vue
+++ src/views/Certificate.vue
@@ -1,6 +1,61 @@
 <template>
-    <div>
-            seraj
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-title">
+                    <h1>From Techniqual college of tabriz</h1>
+                        <div class="card-body">
+                            <img src="../../src/assets/college1.png" alt="college1" class="rounded mx-auto">
+                            <a href="https://tct.ac.ir/" class="btn btn-info d-block">See more</a>
+                        </div>
+                        <div class="card-footer">
+                            from tabriz
+                        </div>
+                </div>
+             </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-title">
+                    <h1>From Techniqual college of tabriz</h1>
+                        <div class="card-body">
+                            <img src="../../src/assets/college1.png" alt="college1" class="rounded mx-auto">
+                            <a href="https://tct.ac.ir/" class="btn btn-info d-block">See more</a>
+                        </div>
+                        <div class="card-footer">
+                            from tabriz
+                        </div>
+                </div>
+             </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-title">
+                    <h1>From Techniqual college of tabriz</h1>
+                        <div class="card-body">
+                            <img src="../../src/assets/college2.png" alt="college1" class="rounded mx-auto">
+                            <a href="https://www.seraj.ac.ir/" class="btn btn-info d-block">See more</a>
+                        </div>
+                        <div class="card-footer">
+                            from tabriz
+                        </div>
+                </div>
+             </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-title">
+                    <h1>Serah university</h1>
+                        <div class="card-body">
+                            <img src="../../src/assets/college2.png" alt="college1" class="rounded mx-auto">
+                            <a href="https://tct.ac.ir/" class="btn btn-info d-block">See more</a>
+                        </div>
+                        <div class="card-footer">
+                            from tabriz
+                        </div>
+                </div>
+             </div>
+        </div>
     </div>
 </template>
 
